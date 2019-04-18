b8ed8d156afec46d89aef4e5bf14b2306d9077cd
luoxiong <382060748@qq.com>
2019-04-18 12:47:31 +0800

    集成阿里巴巴AndroidCodeUtils

---
+++ app/src/main/java/com/lx/tool/UIUtils.java
@@ -0,0 +1,16 @@
+package com.lx.tool;
+
+/**
+ * @author: luoXiong
+ * @date: 2019/4/18 10:14
+ * @version: 1.0
+ * @desc:
+ */
+public class UIUtils {
+    public static void f1() {
+
+    }
+
+    public static void f2() {
+    }
+}
